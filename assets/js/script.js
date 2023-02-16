416ef2cdb9d34af374831ad56646383011357c1a
Russ-S <rstanleym74@gmail.com>
2023-02-16 12:52:41 +0000

    JS: add trashIcon event listener and remove item function. tested working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,15 @@ const formElement = document.querySelector(".form");
 const inputElement = document.querySelector(".input-text");
 const ulElement = document.querySelector(".todo-list");
 
+
+
+
+///--------------------------------------------------///
+
 ////  Event Listeners - to listen for events 
 //// and trigger functions
 
-/**  When a new item is entered into the input area 
+/**  Enter:  When a new item is entered into the input area 
  *  the event listener is triggered on keying ENTER.
  *  The default "submit" action is prevented and a createItem function is triggered
  */
@@ -16,6 +21,23 @@ formElement.addEventListener("submit", (enter) => {
 })
 
 
+
+
+
+
+/**  Toggle Tickbox: The event listener is triggered when the user clicks on the square or tick icon.
+ *  The click event toggles a class in the list item which triggers CSS to change the icon from a square to a tick and cross out the list item as done.
+ */
+
+
+
+
+
+
+
+
+///-----------------------------------------------------///
+
 ////  Functions - to carry out certain tasks triggered by eventListeners, and other actions
 
 /**  createItem() Creates a new item, 
@@ -25,31 +47,50 @@ formElement.addEventListener("submit", (enter) => {
  * then clears out the input field  
  */
 
-
 function createItem() {
+
     const listElement = document.createElement("li");
+
+
+
+    const tickIcon = document.querySelector(".fa-square-check");
+    const squareIcon = document.querySelector(".fa-square");
+    const liElement = document.querySelector("li");
+
     let inputValue = inputElement.value;
-    const tickIconElement = document.createElement("div");
-    const squareIconElement = document.createElement("div");
-    const trashIconElement = document.createElement("div");
-    const barsIconElement = document.createElement("div");
+
 
     listElement.innerText = inputValue;
 
     ulElement.appendChild(listElement);
-
     inputElement.value = "";
 
+    const squareIconElement = document.createElement("div");
     squareIconElement.innerHTML = `<i class="fa-regular fa-square"></i>`;
-
     listElement.appendChild(squareIconElement);
 
+    const trashIconElement = document.createElement("div");
     trashIconElement.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
-
     listElement.appendChild(trashIconElement);
 
+    const barsIconElement = document.createElement("div");
     barsIconElement.innerHTML = `<i class="fas fa-bars"></i>`;
-
     listElement.appendChild(barsIconElement);
+
+
+    // squareIconElement.addEventListener("click", () => {
+    //     liElement.classList.toggle("ticked");
+
+    //     if (liElement.className === "ticked") {
+    //         squareIconElement.innerHTML = `<i class="fa-sharp fa-solid fa-square-check"></i>`
+    //     } else {
+    //         squareIconElement.innerHTML = `<i class = "fa-regular fa-square"></i>`
+    //     };
+    // });
+
+    trashIconElement.addEventListener("click", () => {
+        listElement.remove();
+    });
+
 
 }
