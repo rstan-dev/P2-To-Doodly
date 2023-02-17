41f308476a855b34e9fff164a781bd2fca4dd4e4
Russ-S <rstanleym74@gmail.com>
2023-02-17 12:07:02 +0000

    JS: create updateLocalStorage() function, create displayTodoList() and connet to createItem() function.  Test on refesh

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,15 @@ const inputElement = document.querySelector(".input-text");
 const ulElement = document.querySelector(".todo-list");
 
 
+displayTodoList();
+
+
 
 
 ///--------------------------------------------------///
-
 ////  Event Listeners - to listen for events 
 //// and trigger functions
+///--------------------------------------------------///
 
 /**  Enter:  When a new item is entered into the input area 
  *  the event listener is triggered on keying ENTER.
@@ -21,24 +24,18 @@ formElement.addEventListener("submit", (enter) => {
 })
 
 
-
-
-
-
-/**  Toggle Tickbox: The event listener is triggered when the user clicks on the square or tick icon.
- *  The click event toggles a class in the list item which triggers CSS to change the icon from a square to a tick and cross out the list item as done.
- */
-
-
-
-
-
-
-
-
 ///-----------------------------------------------------///
+////  FUNCTIONS - to carry out certain tasks triggered by eventListeners, and other actions
+///--------------------------------------------------///
 
-////  Functions - to carry out certain tasks triggered by eventListeners, and other actions
+
+/**  displayTodoList() retrieves the items in localStorage 
+ * it gets the items from the todoList Key, and iterates through each item and passes it to the createItem() function  
+ */
+function displayTodoList() {
+    let storageList = JSON.parse(localStorage.getItem("todoList"));
+    storageList.forEach((item) => createItem(item));
+}
 
 /**  createItem() Creates a new item, 
  * it takes the input value, 
@@ -47,17 +44,17 @@ formElement.addEventListener("submit", (enter) => {
  * then clears out the input field  
  */
 
-function createItem() {
+function createItem(item) {
 
     const listElement = document.createElement("li");
-
-
-
     const tickIcon = document.querySelector(".fa-square-check");
     const squareIcon = document.querySelector(".fa-square");
     const liElement = document.querySelector("li");
 
     let inputValue = inputElement.value;
+
+    if (item) inputValue = item.itemName;
+    if (item && item.ticked) listElement.classList.add("ticked");
 
 
     listElement.innerText = inputValue;
@@ -77,6 +74,11 @@ function createItem() {
     barsIconElement.innerHTML = `<i class="fas fa-bars"></i>`;
     listElement.appendChild(barsIconElement);
 
+    updateLocalStorage();
+
+    /**  Toggle Tickbox: The event listener is triggered when the user clicks on the square or tick icon.
+     *  The click event toggles a class in the list item which triggers CSS to change the icon from a square to a tick and cross out the list item as done.
+     */
     squareIconElement.addEventListener("click", () => {
         listElement.classList.toggle("ticked");
         if (listElement.className === "ticked") {
@@ -84,11 +86,40 @@ function createItem() {
         } else {
             squareIconElement.innerHTML = `<i class = "fa-regular fa-square"></i>`
         };
+
     });
 
+    /**  Delete item using the trash can icon.
+     *   when user clicks on the trash can, the list element is deleted using the remove() function
+     */
     trashIconElement.addEventListener("click", () => {
         listElement.remove();
     });
 
 
 }
+
+/**  updateLocalStorage() - updates the number of items on the list
+ * it selects all the list elements and loops through each list element to create an object
+ *  then pushes into an empty array.
+ * The array is saved to localStorage
+ */
+
+function updateLocalStorage() {
+    const listElements = document.querySelectorAll("li");
+    todoList = [];
+
+    listElements.forEach((listElement) => {
+        todoList.push({
+            itemName: listElement.innerText,
+            ticked: listElement.classList.contains("ticked"),
+        });
+    })
+
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+
+/**  updateListCounter - updates the number of items on the list
+ * it t 
+ */
